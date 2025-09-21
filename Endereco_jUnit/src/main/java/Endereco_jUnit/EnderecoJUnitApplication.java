2a2f1fd403b4d5633d0f88cf764d72a309e44de1
Bernardo de Salles Cruz <168240480+devSalles@users.noreply.github.com>
2025-09-21 10:49:14 -0300

    Primeiro commit subindo o projeto

---
+++ Endereco_jUnit/src/main/java/Endereco_jUnit/EnderecoJUnitApplication.java
@@ -0,0 +1,13 @@
+package Endereco_jUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnderecoJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnderecoJUnitApplication.class, args);
+	}
+
+}
