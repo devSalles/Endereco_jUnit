447f8783b2089e15a3298629219db1a500a321df
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2025-09-27 12:44:44 -0300

    Desenvolvimento de teste do metodo de adição para salvamento de dados e requisições inválidas

--- Endereco_jUnit/src/test/java/Endereco_jUnit/service/EnderecoServiceTest.java
+++ Endereco_jUnit/src/test/java/Endereco_jUnit/service/EnderecoServiceTest.java
@@ -13,6 +13,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import jakarta.validation.*;
+import org.junit.jupiter.api.Test;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
 @ExtendWith(MockitoExtension.class)
 
 class EnderecoServiceTest {
@@ -51,5 +59,53 @@ class EnderecoServiceTest {
         validEndereco.setEstado(validEnderecoRequestDTO.getEstado());
     }
 
+    @Test
+    void add_SalvaEntidadeQuandoTodosOsDadosSaoValidos() {
+        when(enderecoRepository.save(any(Endereco.class))).thenReturn(validEndereco);
+
+        Endereco result = this.enderecoService.add(validEnderecoRequestDTO);
+        assertNotNull(result);
+        assertEquals(validEnderecoRequestDTO.getRua(),result.getRua());
+        assertEquals(validEnderecoRequestDTO.getNumero(),result.getNumero());
+        assertEquals(validEnderecoRequestDTO.getCidade(),result.getCidade());
+        assertEquals(validEnderecoRequestDTO.getEstado(),result.getEstado());
+
+        verify(enderecoRepository,times(1)).save(any(Endereco.class));
+    }
+
+    @Test
+    void add_LancaExcecaoQuandoRequisicaoEstaInvalida()
+    {
+        EnderecoRequestDTO enderecoRequestDTO = new EnderecoRequestDTO();
+        enderecoRequestDTO.setRua("");
+        enderecoRequestDTO.setCidade("");
+        enderecoRequestDTO.setNumero(0);
+        enderecoRequestDTO.setEstado(null);
+
+        Set<ConstraintViolation<EnderecoRequestDTO>> validations = validator.validate(enderecoRequestDTO);
+
+        assertFalse(validations.isEmpty());
+        validations.forEach(v-> System.out.println(v.getPropertyPath() + " - " +v.getMessage()));
+    }
+
+    @Test
+    void editById() {
+    }
+
+    @Test
+    void showAll() {
+    }
+
+    @Test
+    void showById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
 
 }
