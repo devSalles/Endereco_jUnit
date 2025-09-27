9a849770850a776a5c07c28cc479b388c46a1a92
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2025-09-27 12:43:01 -0300

    Configuração da classe de teste realizada

--- Endereco_jUnit/src/test/java/Endereco_jUnit/service/EnderecoServiceTest.java
+++ Endereco_jUnit/src/test/java/Endereco_jUnit/service/EnderecoServiceTest.java
@@ -1,4 +1,55 @@
-import static org.junit.jupiter.api.Assertions.*;
+package Endereco_jUnit.service;
+
+import Endereco_jUnit.Enum.EnderecoUF;
+import Endereco_jUnit.dto.EnderecoRequestDTO;
+import Endereco_jUnit.model.Endereco;
+import Endereco_jUnit.repository.EnderecoRepository;
+import jakarta.validation.Validation;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import jakarta.validation.*;
+@ExtendWith(MockitoExtension.class)
+
 class EnderecoServiceTest {
-  
+
+    @Mock
+    private EnderecoRepository enderecoRepository;
+
+    @InjectMocks
+    private EnderecoService enderecoService;
+
+    private Endereco validEndereco;
+    private EnderecoRequestDTO validEnderecoRequestDTO;
+    private static Validator validator;
+
+    @BeforeAll
+    static void setup_Validator()
+    {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator=validatorFactory.getValidator();
+    }
+
+    @BeforeEach
+    void setup()
+    {
+        validEnderecoRequestDTO = new EnderecoRequestDTO();
+        validEnderecoRequestDTO.setCidade("Belo Horizonte");
+        validEnderecoRequestDTO.setRua("São Paulo");
+        validEnderecoRequestDTO.setNumero(166);
+        validEnderecoRequestDTO.setEstado(EnderecoUF.MG);
+
+        validEndereco = new Endereco();
+        validEndereco.setId(1L);
+        validEndereco.setRua(validEnderecoRequestDTO.getRua());
+        validEndereco.setCidade(validEnderecoRequestDTO.getCidade());
+        validEndereco.setNumero(validEnderecoRequestDTO.getNumero());
+        validEndereco.setEstado(validEnderecoRequestDTO.getEstado());
+    }
+
+
 }
